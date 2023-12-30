c4cd09960380cfa8e888aa11fbdc7beddeef43ab
Abdeldjalil Fradj <abdeldjalil.fradj@gmail.com>
2023-12-30 20:04:50 +0100

    AF : added initial function to create ec2 intances

--- aws_utils.py
+++ aws_utils.py
@@ -4,3 +4,38 @@ from os import path
 
 EC2_RESOURCE = boto3.resource('ec2')
 EC2_CLIENT = boto3.client('ec2')
+
+def create_ec2(instance_type, sg_id, key_name, user_data, instance_name):
+    """Creates an EC2 instance
+
+    Args:
+        instance_type (str): Instance type (m4.large, ...)
+        sg_id (str): Security group ID
+        key_name (str): SSH key name
+
+    Returns:
+        instance: The created instance object
+    """
+    instance = EC2_RESOURCE.create_instances(
+        ImageId='ami-0149b2da6ceec4bb0',
+        MinCount=1,
+        MaxCount=1,
+        UserData=user_data,
+        InstanceType=instance_type,
+        Monitoring={'Enabled': True},
+        SecurityGroupIds=[sg_id],
+        KeyName=key_name,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': instance_name
+                    },
+                ]
+            },
+        ]
+    )[0]
+    print(f'{instance} is starting')
+    return instance
